b3c6a004e11fe5f03554ca3d65ccf9ee7b024d24
Ireton, Doug <doug.ireton@nordstrom.com>
2014-10-06 21:47:18 -0700

    Add unit tests for libevent source install

--- spec/recipes/_libevent_spec.rb
+++ spec/recipes/_libevent_spec.rb
@@ -1,9 +1,9 @@
 describe 'tmux::_libevent' do
   let(:libevent_tarball) { "#{Chef::Config['file_cache_path']}/libevent-2.0.21-stable.tar.gz" }
-  let(:chef_run) { ChefSpec::Runner.new(platform: 'centos', version: '6.5') }
 
-  context 'when installing tmux on a RHEL family system' do
-    context 'when libevent install_method is package' do
+  context 'when libevent install_method is package' do
+    context 'on a RHEL family system' do
+      let(:chef_run) { ChefSpec::Runner.new(platform: 'centos', version: '6.5') }
 
       it 'installs the libevent package' do
         chef_run.node.set['tmux']['libevent']['install_method'] = 'package'
@@ -12,20 +12,33 @@ describe 'tmux::_libevent' do
       end
     end
 
-    context 'when libevent install_method is source' do
+    context 'on a Debian family system' do
 
-      it 'downloads the libevent tarball' do
-        chef_run.node.set['tmux']['libevent']['install_method'] = 'source'
+      let(:chef_run) { ChefSpec::Runner.new(platform: 'ubuntu', version: '14.04') }
+
+      it 'installs the libevent-dev package' do
         chef_run.converge(described_recipe)
-        expect(chef_run).to create_remote_file(libevent_tarball)
+        expect(chef_run).to install_package('libevent-dev')
       end
+    end
+  end
 
-      it 'notifies bash to untar and install libevent' do
-        chef_run.converge(described_recipe)
-        libevent_download = chef_run.remote_file(libevent_tarball)
-        expect(libevent_download).to notify('bash[install_libevent]').to(:run).immediately
-      end
+  context 'when libevent install_method is source' do
+    let(:chef_run) { ChefSpec::Runner.new(platform: 'centos', version: '6.5').converge(described_recipe) }
 
+    it 'downloads the libevent tarball' do
+      chef_run.node.set['tmux']['libevent']['install_method'] = 'source'
+      expect(chef_run).to create_remote_file(libevent_tarball)
+    end
+
+    it 'notifies bash to untar and install libevent' do
+      libevent_download = chef_run.remote_file(libevent_tarball)
+      expect(libevent_download).to notify('bash[install_libevent]').to(:run).immediately
+    end
+
+    it 'bash install_libevent only installs when notified' do
+      install_libevent = chef_run.bash('install_libevent')
+      expect(install_libevent).to do_nothing
     end
   end
 end
